94d4cae19266da24433924001860adc5f476936a
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-06 15:05:05 +0100

    lab complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      students = Student.where(first_name: params[:name].capitalize).or(Student.where(last_name: params[:name].capitalize))
+      render json: students
+    else
+      render json: Student.all
+    end
   end
 
+
+  def show
+    render json: Student.find(params[:id])
+  end
 end
